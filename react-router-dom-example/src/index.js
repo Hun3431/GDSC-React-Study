506916f2af2b450e56adb84b645d1f9d7b73a01b
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-05-03 13:04:33 +0900

    Feat react-router-dom-example

---
+++ react-router-dom-example/src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+      Home...
+    </div>
+  )
+}
+
+const Topics = () => {
+  return (
+    <div>
+      <h2>Topics</h2>
+      Topics...
+    </div>
+  )
+}
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact</h2>
+      Contact...
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+      <div>
+        <h1>Hello React Router DOM</h1>
+        <Home></Home>
+        <Topics></Topics>
+        <Contact></Contact>
+      </div>
+  );
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
+
+// ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
