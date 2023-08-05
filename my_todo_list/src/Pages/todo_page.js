f209e7e51241b5469d7ba68f70dc835738bb0736
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-08-05 20:26:31 +0900

    Feat my_todo_list localstorage 저장

--- my_todo_list/src/Pages/todo_page.js
+++ my_todo_list/src/Pages/todo_page.js
@@ -1,21 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ItemList } from "../Components/ItemList_Component";
 import { ItemInput } from "../Components/ItemInput_Component";
 
 const Todo = () => {
   // 1. 로컬스토리지로 데이터 유지할 예정
   // 2. 이후 Firebase로 사용자 데이터 저장 예정
-  const [contents, setContents] = useState([
-    { id: 1, content: "test1", check: false },
-    { id: 2, content: "test2", check: true },
-    { id: 3, content: "test3", check: false },
-  ]);
-  const [nextId, setNextId] = useState(4);
+  const [contents, setContents] = useState("");
+  const [nextId, setNextId] = useState(0);
+
+  const [mounted, setMounted] = useState(true);
+
+  useEffect(() => {
+    // 최초 실행
+    if (mounted) {
+      setMounted(false);
+
+      const lastData = localStorage.getItem("todoList");
+      const lastId = localStorage.getItem("nextId");
+
+      if (lastData && lastId) {
+        setContents(JSON.parse(lastData));
+        setNextId(Number(lastId));
+      }
+    } else {
+      saveTodo();
+    }
+  }, [contents, nextId]);
+
+  // console.log(JSON.parse(lastData));
+  // console.log(JSON.parse(lastId));
+
+  const saveTodo = () => {
+    localStorage.setItem("todoList", JSON.stringify(contents));
+    localStorage.setItem("nextId", nextId);
+  };
 
   const updateTodo = (id) => {
     const newContents = [];
+
     for (let i = 0; i < contents.length; i++) {
       let tempContent = contents[i];
+
       if (tempContent.id == id) {
         tempContent.check = !tempContent.check;
       }
@@ -27,7 +52,9 @@ const Todo = () => {
   const createTodo = (content) => {
     const newContent = { id: nextId, content, check: false };
     const tempContent = [...contents];
+
     tempContent.push(newContent);
+
     setContents(tempContent);
     setNextId(nextId + 1);
   };
