1a0b8d187241a22f0835bc0e3ed07bc3d50373bf
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-08-05 23:59:51 +0900

    Style my_todo_list font

--- my_todo_list/src/Styles/StyledApp.js
+++ my_todo_list/src/Styles/StyledApp.js
@@ -1,14 +1,6 @@
 import styled, { css } from "styled-components";
 
 const StyledApp = styled.div`
-  @font-face {
-    font-family: "omyu_pretty";
-    src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-01@1.0/omyu_pretty.woff2")
-      format("woff2");
-    font-weight: normal;
-    font-style: normal;
-  }
-  
   width: 100%;
 
   display: flex;
@@ -19,8 +11,6 @@ const StyledApp = styled.div`
   -moz-user-select: none;
   -ms-user-select: none;
   user-select: none;
-
-  font-family: "omyu_pretty";
 `;
 
 export { StyledApp };
